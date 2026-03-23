88593199c935752d98c8cc4fa6c5ad3bc370c6c3
Marouane Lemghari <kilpom245@gmail.com>
2026-03-23 17:03:32 +0100

    refactor resume drapdown component

---
+++ components/resume-dropdown.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {ChevronDown} from "lucide-react"
+import Link from "next/link";
+
+const resumeEnglishLink = process.env.RESUME_ENGLISH;
+const resumeFrenchLink = process.env.RESUME_FRENCH;
+
+const ResumeDropdown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        className="flex items-center gap-1 text-sm font-medium hover:text-accent-foreground outline-none"
+        onPointerDown={(e) => {
+          if (e.pointerType === "touch") e.preventDefault();
+        }}
+      >
+        Resume <ChevronDown className="h-4 w-4" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        side="bottom"
+        align="start"
+        className="bg-background border-none"
+      >
+        <DropdownMenuItem asChild>
+          <Link
+            href={resumeEnglishLink || "#"}
+            download
+            className="cursor-pointer"
+          >
+            English
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          <Link
+            href={resumeFrenchLink || "#"}
+            download
+            className="cursor-pointer"
+          >
+            French
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ResumeDropdown;
